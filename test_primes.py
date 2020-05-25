35bfaa18b386a2d513a5718db84f1c61246d894f
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2020-05-25 17:53:44 +0200

    added larger test case, apparently atkin is incorrect

--- test_primes.py
+++ test_primes.py
@@ -1,6 +1,7 @@
 from primes import *
 
 below50 = {2, 3, 5, 37, 7, 41, 11, 43, 13, 47, 17, 19, 23, 29, 31}
+below_million_count = 78498
 
 
 def test_correctness_sieve_of_erastothenes():
@@ -25,3 +26,27 @@ def test_correctness_sieve_of_atkin():
     res_below50 = sieve_of_atkin(50)
 
     assert(res_below50 == below50)
+
+
+def test_large_correctness_sieve_of_erastothenes():
+    count = len(sieve_of_eratosthenes(10**6))
+
+    assert(count == below_million_count)
+
+
+def test_large_correctness_all_odd_to_sqrt():
+    count = len(all_odd_to_sqrt(10**6))
+
+    assert(count == below_million_count)
+
+
+def test_large_correctness_all_6k_plus_minus_1():
+    count = len(all_6k_plus_minus_1(10**6))
+
+    assert(count == below_million_count)
+
+
+def test_large_correctness_sieve_of_atkin():
+    count = len(sieve_of_atkin(10**6))
+
+    assert(count == below_million_count)
